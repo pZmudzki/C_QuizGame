ec0a0bceebe6b7be6351507302b72879a2810da7
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-03-21 00:56:59 +0100

    comment menu file

--- menu.c
+++ menu.c
@@ -6,11 +6,15 @@
 int menu(){
     system("@cls||clear"); // initial console clear to prevent colliding with another locations
 
+    // currSelected - keeps the value of currently selected menu option
     int currSelected = 0;
 
     OutputMenuOptions(currSelected);
     while(1){
-        if (kbhit()) { // for each key press render new view
+        // for each key press render new view
+        // example:
+        // if a user presses arrow key then this loop iterates over with updated view
+        if (kbhit()) {
             int pressed_key = checkKey(); // check what key has been pressed
             switch(pressed_key){
                 case 72: // ascii code for up key
@@ -24,6 +28,7 @@ int menu(){
                     }
                     break;
                 case 13: // ascii code for enter key
+                    // return selected option
                     return currSelected;
             }
             system("@cls||clear"); // clean console
@@ -32,10 +37,10 @@ int menu(){
     }
 }
 
+// function displaying options to a user
 void OutputMenuOptions(int selected){
     char menuOptions[3][12] = {"Start game", "Your stats", "Exit"};
     printf("Welcome, please select what you want to do:\n\n");
-    //printf("%s\n\n", menuOptions[]);
     for(int i = 0; i <= 2; i++){ // list options
         if(selected == i){
             printf("> %s\n", menuOptions[i]);
